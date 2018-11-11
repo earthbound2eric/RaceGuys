1c9f89cc75d7009aa53b7d38a66dccc5e7f53d2e
A-Wynne <andrew.wynne2@gmail.com>
2018-11-11 18:43:36 -0500

    Version1

---
+++ New Unity Project/Assets/Death.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour {
+    public bool hasDied;
+	// Use this for initialization
+	void Start () {
+        hasDied = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.y<-7)
+        {
+            hasDied = true;
+
+        }
+        if(hasDied==true)
+        {
+            StartCoroutine("Die");
+        }
+	}
+    IEnumerator Die()
+    {
+        SceneManager.LoadScene("Level_1");
+        yield return null;
+    }
+}
---
+++ New Unity Project/Assets/PlayerMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 1000;
+    public float moveX;
+    public bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMove1();
+	}
+    void PlayerMove1()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump") == true && isGrounded==true)
+        {
+            Jump();
+        }
+        if(moveX<0.0f&&facingRight==false)
+        {
+            FlipPlayer();
+        }
+        else if(moveX>0.0f&&facingRight==true)
+        {
+            FlipPlayer();
+        }
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        isGrounded = false;
+    }
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag =="ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
+
