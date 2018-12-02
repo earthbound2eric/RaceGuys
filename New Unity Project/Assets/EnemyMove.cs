bda3e64566c918535af8c482a4da010e3a2f7277
A-Wynne <andrew.wynne2@gmail.com>
2018-12-01 21:51:11 -0500

    Level 3 done

---
+++ New Unity Project/Assets/EnemyMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+    
+    private bool facingRight = false;       
+    Vector3 lastposition;
+    Vector3 deltax;
+   
+    // Use this for initialization
+    void Start () {
+        lastposition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deltax = transform.position - lastposition;
+        float test = deltax.x;
+        float test2 = lastposition.x;
+
+        if (test > 0 && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (test < 0 && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        lastposition = transform.position;
+    }
+    
+   void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
